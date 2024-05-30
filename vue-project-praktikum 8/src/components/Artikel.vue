365c399d3fba4e91cb9e019bd2575c58f981bc8e
faizdk <faizdaffakurnia@gmail.com>
2024-05-30 14:55:18 +0700

    Praktikum 8

---
+++ vue-project-praktikum 8/src/components/Artikel.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>ini adalah komponen artikel</h1>
+        <p>Ini adalah isi paragraf</p>
+        <img src="https://assets.promediateknologi.id/crop/0x121:768x664/750x500/webp/photo/2022/02/09/617204399.jpg" alt="images">
+    </div>
+</template>
---
+++ vue-project-praktikum 8/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from '../components/Artikel.vue'
+</script>
+
+<template>
+  <main>
+    <HelloWorld />
+  </main>
+</template>
